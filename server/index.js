a0acff0192c86482dbd7e909a67eb9a27ca3676d
DJ <jeenandra.2003@gmail.com>
2026-04-19 07:56:25 +0530

    chore: prepare for production deployment (PostgreSQL and static serving)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const authRoutes = require('./routes/auth');
 const transactionRoutes = require('./routes/transactions');
@@ -22,6 +23,17 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/goals', goalRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/family', familyRoutes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  
+  app.get('*', (req, res) => {
+    if (!req.path.startsWith('/api')) {
+      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    }
+  });
+}
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
